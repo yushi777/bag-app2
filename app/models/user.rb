13372d486a85cde5c4f2f0b0e1c56f046c20d75a
yushi777 <ucy77777@gmail.com>
2023-04-18 23:53:48 +0900

    ロゴの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid'
+  validates :nick_name, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/ }
+  validates :read_first_name, presence: true, format: { with: /\A[ァ-ヶ一ー]+\z/ }
+  validates :read_last_name, presence: true, format: { with: /\A[ァ-ヶ一ー]+\z/ }
+  validates :age, presence: true
+
 end
