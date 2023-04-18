992923dc6605a3a7680f6a1967ac7c8b1e27608f
yushi777 <ucy77777@gmail.com>
2023-04-18 23:27:00 +0900

    マイグレーションファイルの編集

--- db/migrate/20230417100004_devise_create_users.rb
+++ db/migrate/20230417100004_devise_create_users.rb
@@ -4,8 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string  :email,              null: false, default: ""
+      t.string  :encrypted_password, null: false, default: ""
+      t.string  :nick_name,          null: false, default: ""
+      t.string  :first_name,         null: false, default: ""
+      t.string  :last_name,          null: false, default: ""
+      t.string  :read_first_name,    null: false, default: ""
+      t.string  :read_last_name,     null: false, default: ""
+      t.integer :age,               null: false
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,12 @@ ActiveRecord::Schema.define(version: 2023_04_17_100004) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nick_name", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
+    t.string "read_first_name", default: "", null: false
+    t.string "read_last_name", default: "", null: false
+    t.integer "age", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
